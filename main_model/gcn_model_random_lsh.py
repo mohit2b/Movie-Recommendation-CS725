0293a47514130be698d5f770f403c6bf63e0d6bc
Deeksha Koul <deekshakoul.daiict@gmail.com>
2024-11-26 01:39:44 +0530

    Update gcn_model_random_lsh.py

--- main_model/gcn_model_random_lsh.py
+++ main_model/gcn_model_random_lsh.py
@@ -8,12 +8,12 @@ from collections import defaultdict
 from torch_geometric.nn import GCNConv, GATConv
 import numpy as np
 import random
-
 import time
 
 cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
 
-PATH = "path to save model"
+PATH = "path to load the model"
+model_type = "GCN"
 
 seed = 42
 np.random.seed(seed)
@@ -21,8 +21,6 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 random.seed(seed)
 
-loss_name = "bpr"
-model_type = "GCN"
 
 movie_lens = MovieLens100K('./data/movie_lens')[0]
 movie_features = movie_lens["movie"]["x"]
@@ -33,7 +31,7 @@ data_edge_index = data["edge_index"][:, mask]
 data_edge_label = data["rating"][mask]
 
 user_num_nodes = user_features.shape[0]
-train_nodes, testing_nodes = train_test_split(range(user_num_nodes), test_size=0.2, random_state=42)
+train_nodes, testing_nodes = train_test_split(range(user_num_nodes), test_size=0.2, random_state=seed)
 val_nodes, test_nodes = testing_nodes[:len(testing_nodes)//2], testing_nodes[len(testing_nodes)//2: ]
 
 Y = data_edge_index[0]
@@ -80,108 +78,9 @@ class GNN(torch.nn.Module):
         z = self.conv2(z, edge_index)
         return z
     
-
-def bpr_loss(all_embeds, pos_edge_index, num_neg_samples=50):
-    loss = 0.0
-    visited_nodes = set()
-
-    for i in range(pos_edge_index.shape[1]):
-        u, v = pos_edge_index[:, i]  # Extract a positive edge (u -> v)
-        if u in visited_nodes:
-            continue
-        visited_nodes.add(u)
-
-        u_embedding = all_embeds[u].unsqueeze(0)
-        pos_nbr_embedding = all_embeds[pos_edge_index[1][pos_edge_index[0] == u]]
-
-        neg_nbors_indices = sample_neg_edges(u, pos_edge_index, num_sample=1)
-        neg_nbrs = all_embeds[neg_nbors_indices]
-
-        pos_score = cos(pos_nbr_embedding, u_embedding) 
-        neg_scores = cos(neg_nbrs, u_embedding)  
-
-        # BPR loss: log-sigmoid of the score differences
-        bpr_terms = -F.logsigmoid(pos_score - neg_scores)  
-        loss += bpr_terms.sum()
-
-    return loss/pos_edge_index.shape[1]
-
-cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-def margin_loss(all_embeds, pos_edge_index, margin):
-    loss = 0.0
-    visited_nodes = set()
-    for i in range(pos_edge_index.shape[1]):
-        u, v = pos_edge_index[:, i]
-        if u in visited_nodes:
-            continue
-        visited_nodes.add(u)
-        u_embedding = all_embeds[u].unsqueeze(0)
-
-        neg_nbors_indices = sample_neg_edges(u, pos_edge_index, num_sample = 15)
-
-        # Positive neighbors - all v where u -> v
-        pos_nbrs = all_embeds[pos_edge_index[1][pos_edge_index[0] == u]]
-
-        # Negative neighbors - all v where u->v in negative sampling
-        neg_nbrs = all_embeds[neg_nbors_indices]
-
-        pos_scores = cos(pos_nbrs, u_embedding)  
-        neg_scores = cos(neg_nbrs, u_embedding)  
-
-        len_pos = pos_scores.shape[0]
-        len_neg = neg_scores.shape[0]
-
-        expanded_pos_scores = pos_scores.unsqueeze(1).expand(len_pos, len_neg)
-        expanded_neg_scores = neg_scores.unsqueeze(0).expand(len_pos, len_neg)
-        import pdb; pdb.set_trace()
-        # Margin-based loss calculation with max function
-        margin_penalty = torch.max(margin + expanded_neg_scores - expanded_pos_scores, torch.tensor(0.0, device=all_embeds.device))
-        loss += margin_penalty.sum()
-
-    return loss/pos_edge_index.shape[1]
-
 model = GNN(model_type, in_channels= 32, hidden_channels=128, out_channels=64)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 model=model.to(device)
-
-def sample_neg_edges(curr_node, edge_index, num_sample):
-  mask = torch.ones(edge_index[1].shape[0], dtype = torch.bool)
-  pos_items = edge_index[1][edge_index[0] == curr_node]
-  mask[pos_items] = False
-  assert mask.shape == edge_index[1].shape
-  val = edge_index[1][mask][:num_sample]
-  return val
-
-def negative_sample_calculation(nodes, edge_index):
-  neg_sample_dict = defaultdict()
-  for i in tqdm(range(len(nodes))):
-    neg_val = sample_neg_edges(nodes[i], edge_index, num_sample=1000)
-    neg_sample_dict[nodes[i]] = neg_val
-
-  return neg_sample_dict
-
-
-min_ep_loss = 1e6
-num_nodes = train_edge_index.max().item() + 1
-EPOCHS = 1
-
-for i in tqdm(range(EPOCHS)):
-    model.train()
-    total_loss = 0.0
-    optimizer.zero_grad()
-    embeddings = model(user_features, movie_features, train_edge_index)
-    if loss_name == "bpr":
-        loss = bpr_loss(embeddings, train_edge_index)
-    if loss_name == "margin_loss":
-        loss = margin_loss(embeddings, train_edge_index, margin = 0.01)
-
-    if loss < min_ep_loss:
-
-        min_ep_loss = loss
-        torch.save(model.state_dict(), PATH)
-    loss.backward()
-    optimizer.step()
-
 model.load_state_dict(torch.load(PATH))
 
 model.eval()
@@ -269,19 +168,11 @@ class RandomProjection:
         p_1 =  1 if top_k[0] in test_edges_q_indices else 0
         p1.append(p_1 / 1)
         r10.append(p_10/len(test_edges_q_indices))
-
-        # if p_10 > 0:
-        #   print(curr_node, "user ")
-        #   print(similarities.shape)
-        #   print(sorted_indices[:10])
-        #   print("p10---> ", p_10)
-        #   break
         for rank, node in enumerate(top_k):
             if node in test_edges_q_indices:
                 mrr.append(1 / (rank + 1))
                 break
 
-        # NDCG Calculation
         dcg = 0.0
         for rank, node in enumerate(top_k, start=1):
             if node in test_edges_q_indices:
